0163b91a5d736ce09756bd33b724b32012aaf218
inovarka <paveltimofeenko@gmail.com>
2021-06-04 12:45:02 +0300

    Change package names

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,8 +8,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/roman-mazur/design-practice-2-template/httptools"
-	"github.com/roman-mazur/design-practice-2-template/signal"
+	"github.com/inovarka/se-lab2/httptools"
+	"github.com/inovarka/se-lab2/signal"
 )
 
 var port = flag.Int("port", 8080, "server port")
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/roman-mazur/design-practice-2-template
+module github.com/inovarka/se-lab2
 
-go 1.15
+go 1.16
