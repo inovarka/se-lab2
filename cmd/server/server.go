b259242ea81be1b6ebe88ea013bb6155a449521a
inovarka <paveltimofeenko@gmail.com>
2021-06-05 16:31:21 +0300

    Change server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,56 +1,85 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/inovarka/se-lab2/httptools"
 	"github.com/inovarka/se-lab2/signal"
 )
 
-var port = flag.Int("port", 8080, "server port")
-
-const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
-const confHealthFailure = "CONF_HEALTH_FAILURE"
+const (
+	confHealthFailure = "CONF_HEALTH_FAILURE"
+)
 
 func main() {
-	h := new(http.ServeMux)
+	port := flag.Int(
+		"port",
+		8080,
+		"server port",
+	)
 
-	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Set("content-type", "text/plain")
-		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, _ = rw.Write([]byte("FAILURE"))
-		} else {
-			rw.WriteHeader(http.StatusOK)
-			_, _ = rw.Write([]byte("OK"))
-		}
-	})
+	flag.Parse()
+
+	h := http.NewServeMux()
+
+	h.HandleFunc(
+		"/health",
+		func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("content-type", "text/plain")
+			if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
+				rw.WriteHeader(http.StatusInternalServerError)
+				_, _ = rw.Write([]byte("FAILURE"))
+			} else {
+				rw.WriteHeader(http.StatusOK)
+				_, _ = rw.Write([]byte("OK"))
+			}
+		},
+	)
 
 	report := make(Report)
 
-	h.HandleFunc("/api/v1/some-data", func(rw http.ResponseWriter, r *http.Request) {
-		respDelayString := os.Getenv(confResponseDelaySec)
-		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
-			time.Sleep(time.Duration(delaySec) * time.Second)
-		}
+	h.HandleFunc(
+		"/api/v1/some-data",
+		func(rw http.ResponseWriter, r *http.Request) {
+			key := r.FormValue("key")
+			if key == "" {
+				rw.WriteHeader(http.StatusNotFound)
 
-		report.Process(r)
+				return
+			}
 
-		rw.Header().Set("content-type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		_ = json.NewEncoder(rw).Encode([]string{
-			"1", "2",
-		})
-	})
+			reqURL := fmt.Sprintf("http://db:8080/db/%s", key)
+
+			resp, err := http.Get(reqURL)
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+
+				return
+			}
+
+			for k, values := range resp.Header {
+				for _, value := range values {
+					rw.Header().Add(k, value)
+				}
+			}
+
+			rw.WriteHeader(resp.StatusCode)
+			defer resp.Body.Close()
+
+			if _, err = io.Copy(rw, resp.Body); err != nil {
+				return
+			}
+		},
+	)
 
 	h.Handle("/report", report)
 
 	server := httptools.CreateServer(*port, h)
 	server.Start()
+
 	signal.WaitForTerminationSignal()
 }
